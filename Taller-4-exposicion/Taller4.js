5b2d56128119d460f10c4619049f9c82598d3c74
NataliaTurmeque <nataliaturmeque1018@gmail.com>
2023-05-17 20:27:19 -0500

    Proyecto 1 exposición

---
+++ Taller-4-exposicion/Taller4.js
@@ -0,0 +1,10 @@
+function agregarClase(){
+    const miTitulo = document.getElementById("titulo");
+    miTitulo.classList.add("destacado")
+
+}
+function agregarClase1(){
+    const miTitulo = document.getElementById("titulo1");
+    miTitulo.classList.add("destacado1")
+
+}
